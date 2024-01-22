59c997e35519656ee683a01338f09e2f9728fa50
GoldenCloudPc <jvladimir54@yahoo.com>
2024-01-22 14:32:38 +0100

    Created sterter endpoints

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,6 +1,16 @@
+using GameStore.api.Entities;
+
+List<Game> game = new(){
+    new Game(){ Name = "Street Fighter II", Genre= "Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991,2,1), ImageUri = "https:/placeholder.co/100" }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/home", () => "This is for testing purpeses!");
+
+app.MapGet("/data", () => "{ data:300, samsung:200 }");
 
 app.Run();
