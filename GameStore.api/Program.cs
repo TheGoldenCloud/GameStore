c803f5d64c648ae56388398da9205adcce0064fc
GoldenCloudPc <jvladimir54@yahoo.com>
2024-01-24 16:59:31 +0100

    Added core endpoints

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,6 +1,8 @@
 using GameStore.api.Entities;
 
-List<Game> game = new(){    //In memory
+const string GetGameEndpointName = "GetGame";
+
+List<Game> games = new(){    //In memory
     new Game(){ Id = 1, Name = "Street Mortal combat", Genre= "Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991,2,1), ImageUri = "https:/placeholder.co/100" },
     new Game(){ Id = 2, Name = "Street Final Fantasy XIV", Genre= "Roleplaying", Price = 59.99M, ReleaseDate = new DateTime(2010,9,30), ImageUri = "https:/placeholder.co/100" },
     new Game(){ Id = 3, Name = "Street Pes 10", Genre= "Sports", Price = 69.99M, ReleaseDate = new DateTime(2022,9,27), ImageUri = "https:/placeholder.co/100" }
@@ -15,22 +17,63 @@ app.MapGet("/home", () => "This is for testing purpeses!");
 
 app.MapGet("/game/{id}", (int id) =>
 {
-    Game? game_ = game.Find(game => game.Id == id);
+    Game? game_ = games.Find(game => game.Id == id);
 
     if (game_ is null)
     {
         return Results.NotFound();
     }
 
-    return Results.Ok(game);
+    return Results.Ok(games);
+}).WithName(GetGameEndpointName);
+
+app.MapPost("/games", (Game game) =>
+{
+    game.Id = games.Max(game => game.Id) + 1;
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
+
+app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+{
+    Game? existingGame = games.Find(game => game.Id == id);
+
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.ImageUri = updatedGame.ImageUri;
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is not null)
+    {
+        games.Remove(game);
+    }
+
+    return Results.NoContent();
+});
+
+
+
 
 string gamess()
 {
     string allgames = "";
-    for (int i = 0; i <= game.Count - 1; i++)
+    for (int i = 0; i <= games.Count - 1; i++)
     {
-        allgames += "Name: " + game[i].Name + " price: " + game[i].Price + "\n";
+        allgames += "Name: " + games[i].Name + " price: " + games[i].Price + "\n";
     }
     return allgames;
 }
