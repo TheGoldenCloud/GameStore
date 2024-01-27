8e22f6dfca4e8b00d9411e6978590f678bdd4683
GoldenCloudPc <jvladimir54@yahoo.com>
2024-01-27 23:30:08 +0100

    Added services

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,7 +1,10 @@
 using GameStore.api.Entities;
 using GameStore.api.Endpoints;
+using GameStore.api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IGamesRepository, InMemGamesRepository>();
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
