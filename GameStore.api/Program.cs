99ba3017852e658fccf600ebc947c260227f4e29
GoldenCloudPc <jvladimir54@yahoo.com>
2024-02-03 19:04:58 +0100

    Added server

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,11 +1,13 @@
 using GameStore.api.Entities;
 using GameStore.api.Endpoints;
 using GameStore.api.Repositories;
+using GameStore.api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 
 var constr = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddSqlServer<GameStoreContext>(constr);
 
 var app = builder.Build();
 
