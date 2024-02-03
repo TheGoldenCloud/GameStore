d2749686ae87e728519ae142d960687353dfad7b
GoldenCloudPc <jvladimir54@yahoo.com>
2024-02-03 18:51:38 +0100

    Base

--- GameStore.api/Data/GameStoreContext.cs
+++ GameStore.api/Data/GameStoreContext.cs
@@ -1,8 +1,14 @@
-using Microsoft;
+using GameStore.api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.api.Data;
 
 public class GameStoreContext : DbContext
 {
+    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+    {
 
+    }
+
+    public DbSet<Game> Games => Set<Game>();
 }
