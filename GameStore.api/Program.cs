63c3436e85cc622e7c4aa0300f956bf053513c76
GoldenCloudPc <jvladimir54@yahoo.com>
2024-02-05 19:01:43 +0100

    Added intializing db

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -2,6 +2,7 @@ using GameStore.api.Entities;
 using GameStore.api.Endpoints;
 using GameStore.api.Repositories;
 using GameStore.api.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
@@ -10,6 +11,14 @@ var constr = builder.Configuration.GetConnectionString("GameStoreContext");
 builder.Services.AddSqlServer<GameStoreContext>(constr);
 
 var app = builder.Build();
+
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbcontext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+//     dbcontext.Database.Migrate();
+// }
+
+app.Services.InitializeDb();
 
 app.MapGamesEndpoints();
 
