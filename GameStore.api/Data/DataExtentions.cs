a7cc3474af55fd211dddb32e75d66b05b1b5f3b2
GoldenCloudPc <jvladimir54@yahoo.com>
2024-02-08 11:29:03 +0100

    Update

--- GameStore.api/Data/DataExtentions.cs
+++ GameStore.api/Data/DataExtentions.cs
@@ -10,6 +10,14 @@ public static class DataExtentions
         var dbcontext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbcontext.Database.Migrate();
     }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        var constr = configuration.GetConnectionString("GameStoreContext");
+        services.AddSqlServer<GameStoreContext>(constr).AddScoped<IGamesRepository, EntityFrameworkGamesRepository>();
+
+        return services;
+    }
 }
 
 //Unhandled exception. Microsoft.Data.SqlClient.SqlException (0x80131904): Login failed for user 'sa'.
